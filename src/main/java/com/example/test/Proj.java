43b7a5d5a83674ffe98f8e178c2bbbc5ffb3563a
Anujg22 <94348973+Anujg22@users.noreply.github.com>
2023-07-07 16:38:50 +0530

    Update Proj.java

--- src/main/java/com/example/test/Proj.java
+++ src/main/java/com/example/test/Proj.java
@@ -2,7 +2,7 @@ package com.example.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+#comment
 @RestController
 public class Proj {
     @GetMapping("/myproj")
