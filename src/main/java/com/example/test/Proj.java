d63a6b59b2f1e3b7cdc28a037cf922f2fa8bfe6e
Anuj <anujg0314@gmail.com>
2023-07-13 15:38:36 +0530

    Made changes

--- src/main/java/com/example/test/Proj.java
+++ src/main/java/com/example/test/Proj.java
@@ -2,11 +2,11 @@ package com.example.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-#comment
+
 @RestController
 public class Proj {
     @GetMapping("/myproj")
     public String getData(){
-        return "We are learning devops";
+        return "We are learning devops in AWS";
     }
 }
